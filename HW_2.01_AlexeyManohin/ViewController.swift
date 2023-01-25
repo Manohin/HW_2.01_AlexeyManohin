5bc56440733256dbded99d657200d24a2cef6eae
Manohin <111537419+Manohin@users.noreply.github.com>
2023-01-25 22:38:55 +0300

    Initial Commit

---
+++ HW_2.01_AlexeyManohin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.01_AlexeyManohin
+//
+//  Created by Alexey Manokhin on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
